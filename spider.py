201a78e3fb7ab448df7e4b19bc7a4e8364ffc076
zzsophcz <zzsophcz@gmail.com>
2025-06-07 18:08:05 +0800

    添加爬取逻辑思路

--- spider.py
+++ spider.py
@@ -1,5 +1,22 @@
+from bs4 import BeautifulSoup
+import re
+import urllib.request,urllib.error
+import xlwt
+import sqlite3
+
 def main():
-    print("hello")
+    # 1.爬取网页
+    # 2.解析数据
+    # 3.保存数据
+    baseurl="https://movie.douban.com/top250?start="
+
+def getData():
+    datalist=[]
+    #2.逐一解析
+    return datalist
+
+def saveData():
+    print("在这实现保存数据 ")
 
 if __name__ == '__main__':
     main()
